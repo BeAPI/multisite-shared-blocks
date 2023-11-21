83dd661fd905d88c48bdc981f5f08aea8ddedab1
Clement Boirie <cboirie@beapi.fr>
2023-11-21 14:30:39 +0000

    Release version 1.1.0

---
+++ build/hooks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-wordcount'), 'version' => 'ec9cea5ef0a395114c8a');
